1ad1fafe8dee6d7ee3c36c6a3e1374381151e40e
TesterJo <testerjhy@gmail.com>
2023-07-22 18:50:09 +0900

    04-2_InputOutput, 20230722

---
+++ 04-2_InputOutput.py
@@ -0,0 +1,20 @@
+# input 사용하기
+# a = input()
+# print(a)
+
+# 프롬프트를 띄워 사용자 입력받기
+# number = input('숫자를 입력하세요: ')
+# print('입력된 숫자 출력 : %s' %number)
+# print('number타입 확인 : ' , type(number))
+
+# print 자세히 알기
+# 큰따옴표로 둘러싸인 문자열은 +연산과 동일하다
+print("life" "is" "too short")
+print("life" + "is" + "too short")
+
+# 문자열 띄어쓰기는 쉼표로 한다
+print("life", "is", "too short")
+
+# 한 줄에 결괏값 출력하기
+for i in range(10):
+    print(i, end=' ')
