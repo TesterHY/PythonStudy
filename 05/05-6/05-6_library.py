bc492808982322c9256742b4fe7ab5c51c92de5c
TesterJo <testerjhy@gmail.com>
2023-08-07 23:33:03 +0900

    20230807

--- 05/05-6/05-6_library.py
+++ 05/05-6/05-6_library.py
@@ -73,3 +73,148 @@ print(math.lcm(15, 25))
 import random
 print(random.random()) # 0.0~1.0 난수 리턴
 print(random.randint(1, 100)) # 1~100 난수 리턴
+
+# itertols.zip_longest : 같은 개수의 자료형을 묶는 파이썬 내장 함수인 zip 함수와 똑같이 동작한다.
+# 하지만 itertools.zip_logest() 함수는 전달한 반복 가능 객체(*iterables)의 길이가 서로 다르다면
+# 긴 객체의 길이에 맞춰 fillvalue에 설정한 값을 객체에 채울 수 있다.
+students = ['Sara', 'Shiu', 'HY', 'Aka', 'Jack', 'Bit']
+snacks = ['candy', 'jelly', 'chocolate']
+
+result1 = zip(students, snacks)
+print(list(result1))
+
+import itertools
+result2 = itertools.zip_longest(students, snacks, fillvalue='potato chips')
+print(list(result2))
+
+
+# itertools.permutation
+# itertools.permutation(iterable, r) 은 반복 가능 객체 중에서 r개를 선택한 순열을 이터레이터로 리턴하는 함수
+# 1, 2, 3이라는 숫자가 적힌 3장의 카드에서 2장의 카드를 꺼내 만들 수 있는 2자리 숫자를 모두 구하려면 어떻게 해야 할까?
+# (1,2), (1,3), (2,1), (2,3), (3,1), (3,2) 로 6가지
+result3 = itertools.permutations(['1', '2', '3'], 2)
+print(list(result3))
+
+# [1,2,3]이라느 3장의 카드 중 순서에 상관없이 2장을 뽑는 경우의 수는 모두 3가지(조합)
+# (1,2), (2,3), (1,3)
+result4 = itertools.combinations(['1', '2', '3'], 2)
+print(list(result4))
+
+# 1~45 중 서로 다른 숫자 6개를 뽑는 로또 번호의 모든 경우의 수(조합)를 구하고 그 개수를 출력하려면 어떻게 해야 할까?
+lotto = itertools.combinations(range(1, 46), 6)
+#print(len(list(lotto)))
+
+# 같은 숫자를 허용하는 중복 조합은 
+lotto2 = itertools.combinations_with_replacement(range(1, 46), 6)
+#print(len(list(lotto2)))
+
+#functools.reduce(function, iterable) : 함수(function)를 반복 가능한 객체(iterable)의 요소에 차례대로(왼쪽에서 오른쪽으로)
+# 누적 적용하여 이 객체를 하나의 값으로 줄이는 함수이다.
+def add(data):
+    result = 0
+    for i in data:
+        result += i
+    return result
+data = [1, 2, 3, 4, 5]
+result = add(data)
+print(result)
+
+import functools
+result2 = functools.reduce(lambda x, y: x+y, data)
+print(result2)
+
+# 최댓값 구하기
+num_list = [3, 2, 8, 1, 6, 7]
+max_num = functools.reduce(lambda x, y : x if x > y else y, num_list)
+min_num = functools.reduce(lambda x, y : x if x < y else y, num_list)
+print(max_num, min_num)
+
+# operator.itemgetter : sorted와 같은 함수의 key 매개변수에 적용하여 다양한 기준으로 정렬할 수 있도록 도와주는 모듈
+from operator import itemgetter
+students = [
+    ('jane', 22, 'A'),
+    ('dave', 32, 'B'),
+    ('sally', 12, 'B'),
+]
+
+# 튜플의 요소로 정렬
+result = sorted(students, key=itemgetter(1)) # 두 번째 요소로 정렬
+print(result)
+
+students2 = [
+    {"name": "jane", "age": 22, "grade": 'A'},
+    {"name": "dave", "age": 32, "grade": 'B'},
+    {"name": "sally", "age": 17, "grade": 'B'},
+]
+
+# 딕셔너리 키를 사용하여 정렬
+result = sorted(students2, key=itemgetter('age'))
+print(result)
+
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+students3 = [
+    Student('jane', 22, 'A'),
+    Student('dave', 32, 'B'),
+    Student('sally', 17, 'C'),
+]
+
+# 클래스 객체 정렬 -> attrgetter()
+from operator import attrgetter
+result = sorted(students3, key=attrgetter('age'))
+print(result)
+
+# shutil : 복사하거나 이동할 떄 사용하는 모듈
+import shutil
+# shutil.copy("copy moto", "copy saki")
+# shutil.move("copy moto", "copy saki")
+
+# glob : 특정 디렉토리에 있는 파일들을 리스트로 만들기
+import glob
+filelist = glob.glob("c:/Dev/PythonWorkspace/PythonStudy/05/*.py")
+print(filelist)
+
+# pickle : 객체의 형태를 그대로 유지하면서 파일에 저장하고 불러올 수 있게 하는 모듈
+import pickle
+
+# dump함수를 사용하여 딕셔너리 객체인 data를 그대로 파일에 저장
+f = open("test.txt", 'wb')
+data = {1: 'python', 2: 'you need'}
+pickle.dump(data, f)
+f.close()
+
+# dump로 저장한 파일을 load를 사용해서 원래 있던 딕셔너리 객체 그대로 불러오는 예
+f = open("test.txt", 'rb')
+data = pickle.load(f)
+print(data)
+f.close()
+
+## os : 환경 변수나 디렉터리, 파일 등의 OS 자원을 제어할 수 있게 해 주는 모듈
+# os.environ : 환경변수
+import os
+print(os.environ['PATH'])
+
+
+# os.getcwd : 현재 디렉터리 위치 리턴
+print(os.getcwd())
+
+# os.chdir : 디렉터리 위치 변경
+os.chdir("C:/Dev")
+print(os.getcwd())
+
+# os.system : 시스템 명령어 호출
+print(os.system("dir"))
+
+# os.popen : 실행한 시스템 명렁어의 결괏값 돌려받기
+f = os.popen("dir")
+print(f.read())
+
+# os.mkdir(디렉토리) : 디렉터리 생성
+# os.rmdir(디렉토리) : 디렉터리 삭제. 단, 디렉터리가 비어 있어야 삭제할 수 있다.
+# os.remove(파일) : 파일 삭제
+# os.rename0(src, dst) : src라는 이름의 파일을 dst라는 이름으로 바꾼다.
